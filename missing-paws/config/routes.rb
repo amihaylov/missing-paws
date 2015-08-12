07fb771b8b0007b2fca9eadd918dbcc258050227
GeorqeMilev <GeorqeMilev@gmail.com>
2015-08-12 18:12:34 +0300

    add

--- missing-paws/config/routes.rb
+++ missing-paws/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :articles
   resources :found_animals
   resources :lost_animals
-  resources :stories
-  get 'home/index'
 
+
+
+
+  get 'home/index'
   get 'news/index'
 
 
@@ -12,7 +15,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'news#index'
+  root to: "articles#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- missing-paws/db/schema.rb
+++ missing-paws/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811083451) do
+ActiveRecord::Schema.define(version: 20150811214836) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.text     "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "found_animals", force: :cascade do |t|
     t.string   "title"
@@ -35,14 +43,6 @@ ActiveRecord::Schema.define(version: 20150811083451) do
     t.string   "city"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "stories", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
