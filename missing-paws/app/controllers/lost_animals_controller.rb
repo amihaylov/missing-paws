ebab80c79bf5b931ff7b9c2bc23e896a7b0d6a6f
Tabris841 <shadowtabris84@gmail.com>
2015-08-15 16:43:41 +0300

    More improvments in LostAnimals page

--- missing-paws/app/controllers/lost_animals_controller.rb
+++ missing-paws/app/controllers/lost_animals_controller.rb
@@ -1,5 +1,5 @@
 class LostAnimalsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
   before_action :correct_user,   only: [:show, :edit, :update, :destroy]
 
   # GET /lost_animals
