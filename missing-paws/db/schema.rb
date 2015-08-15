5ad0171ad4eac00c65e9161d4c63530b9c2f0b2e
GeorqeMilev <GeorqeMilev@gmail.com>
2015-08-15 11:42:49 +0300

    Merge branch 'master' of https://github.com/amihaylov/missing-paws
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- missing-paws/db/schema.rb
+++ missing-paws/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813151411) do
+ActiveRecord::Schema.define(version: 20150815041748) do
 
   create_table "adoptions", force: :cascade do |t|
     t.string   "title"
@@ -109,6 +109,16 @@ ActiveRecord::Schema.define(version: 20150813151411) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "vet_centers", force: :cascade do |t|
     t.string   "title"
