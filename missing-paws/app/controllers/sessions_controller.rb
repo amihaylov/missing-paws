b0162dfd64300c3596f5c29fba32525bc9fc63ac
amihaylov <anton.mihaylov7@gmail.com>
2015-09-02 17:24:39 +0300

    Articles are now fully translated.

--- missing-paws/app/controllers/sessions_controller.rb
+++ missing-paws/app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_url
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = I18n.t('.invalid-pwmail') || 'Invalid email/password combination'
       render 'new'
     end
   end
